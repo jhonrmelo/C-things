2ba882bb936aae37c2d6a693bab1a973bb2eb2fe
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2019-10-19 18:36:26 -0300

    Lab 8

---
+++ Recursao/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+int palindromo(char * string, int ini, int fim);
+int main()
+{
+    char strPalindromo[200];
+   printf("Digite a expressao");
+   gets(strPalindromo);
+   int fim = strlen(strPalindromo);
+
+  int retorno =  palindromo(&strPalindromo, 0, fim -1);
+
+
+
+  if(retorno >0){
+    printf("Palindromo");
+  }
+  else {
+    printf("Nao e um palindromo");
+  }
+
+}
+
+int palindromo(char * string, int ini, int fim){
+if(ini == fim){
+    return 1;
+}
+if(ini > fim ){
+    return 1;
+}
+if(string[ini] == ' '){
+   return palindromo(string, ini+1,fim);
+}
+if(string[fim] == ' '){
+   return palindromo(string, ini,fim -1);
+}
+if(string[ini] == string[fim])
+  return palindromo(string, ini+1, fim-1);
+else {
+    return -1;
+}
+
+}
